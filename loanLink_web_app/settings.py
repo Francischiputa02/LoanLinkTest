412bc7306afad8057d96c16cf7830f0092af25e8
Francischiputa02 <163286536+Francischiputa02@users.noreply.github.com>
2024-04-28 09:38:24 +0200

    Update settings.py

--- loanLink_web_app/settings.py
+++ loanLink_web_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'corsheaders',
+    #'corsheaders',
     'django.contrib.staticfiles',
     
 ]
